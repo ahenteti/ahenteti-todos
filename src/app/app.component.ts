f1922295880cd8b84ae2e2ca5a91190d5816d8a8
ahenteti <ahmad.henteti@gmail.com>
2020-07-17 21:14:35 +0200

    implement add/remove todos actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public todos: Todo[] = [];
+  public todoToAdd: string;
+  public errors: string[] = [];
+
+  public addTodo() {
+    this.validateAddTodo();
+    if (this.errors.length > 0) return;
+    this.todos.push({ title: this.todoToAdd });
+    this.todoToAdd = '';
+  }
+
+  public removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  private validateAddTodo() {
+    this.errors = [];
+    if (!this.todoToAdd) {
+      this.errors.push('todo title is required');
+    }
+  }
+}
